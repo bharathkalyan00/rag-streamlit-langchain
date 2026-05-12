0fc5ffc1dc1009b1ef018a6a44c381ed799ee056
Bharath Kalyan <bharathkalyan00@gmail.com>
2026-05-13 01:09:34 +0530

    Initial commit - Streamlit RAG app

---
+++ app.py
@@ -0,0 +1,65 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain_core.runnables import RunnablePassthrough
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+
+@st.cache_resource
+def get_chain():
+    # ingestion
+    pdf = PyPDFLoader("attention.pdf").load()
+
+    # Chunking
+    chunks = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200
+    ).split_documents(pdf)
+    # print(chunks[:20])
+
+    # embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # vector db
+    db = Chroma.from_documents(chunks, embeddings)
+
+    # prompt template
+
+    prompt = ChatPromptTemplate.from_template("""
+        Only answer the given question based on the context. If it is out of scope, just say you don't know"
+        Context : {Context}
+        Question : {Question}
+        """)
+
+    llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+    # retriever
+    retriever = db.as_retriever()
+
+    # chain
+    chain = (
+        {"Context": retriever, "Question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return chain
+
+
+chain = get_chain()
+st.title("QnA Chatbot on Attention is all you need")
+query = st.text_input("Enter what you'd like to know")
+
+if query:
+    res = chain.invoke(query)
+    st.write(res)
